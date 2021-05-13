3813f0269bbca64a83c8bd9c7e08d4522cf8a2dd
SominLeeeeee <somin116@gmail.com>
2021-05-13 17:58:36 +0900

    [navermap] 네이버 지도와 마커 표시

--- src/App.js
+++ src/App.js
@@ -1,28 +1,74 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { RenderAfterNavermapsLoaded, NaverMap, Marker, loadNavermapsScript } from 'react-naver-maps'; // naver map 패키지 불러오기
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+const navermaps = window.naver.maps;
+
+function addMarker(lat, lng) {
+  return (
+    <Marker 
+      position={new navermaps.LatLng(lat, lng)}
+      animation={navermaps.Animation.BOUNCE}
+      onClick={() => {
+        alert("here is naver!")
+      }}
+    />
+  );
+}
+
+const sample = [
+  {
+    lat: 37.554722, lng: 126.970833
+  },
+  {
+    lat: 37.553722, lng: 126.920833
+  },
+  {
+    lat: 37.553722, lng: 126.922833
+  },
+];
+
+function NaverMapAPI() {
+
+  return (
+    <NaverMap
+      mapDivId={'maps-getting-started-uncontrolled'} // default: react-naver-map
+      style={{
+        width: '100vw', // 네이버지도 가로 길이
+        height: '100vh' // 네이버지도 세로 길이
+      }}
+      defaultCenter={{ lat: 37.554722, lng: 126.970833 }} // 지도 초기 위치
+      defaultZoom={15} // 지도 초기 확대 배율
+      >
+
+      {
+        sample.map((pos) => (
+          <Marker 
+            position={new navermaps.LatLng(pos.lat, pos.lng)}
+            onClick={() => {
+              alert("here is naver!")
+            }}
+            icon='http://localhost:3000/marker.jpg'
+            // 'http://localhost:3000/public/marker.png'
+          />
+        ))
+      }
+
+
+    </NaverMap>
+  );
+}
+
+function App() {
+  return (
+    <RenderAfterNavermapsLoaded
+      ncpClientId={'wdd5w8xqhv'} // 자신의 네이버 계정에서 발급받은 Client ID
+      error={<p>Maps Load Error</p>}
+      loading={<p>Maps Loading...</p>}
+    >
+      <NaverMapAPI />
+    </RenderAfterNavermapsLoaded>
+  );
 }
 
 export default App;
